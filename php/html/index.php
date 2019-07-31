f906347a6e4b7ac107ba04a61b5cea23869b63e6
Justin McKee <thisisjustinmckee@gmail.com>
2019-07-31 16:11:36 -0400

    Adds sample sql dump

--- php/html/index.php
+++ php/html/index.php
@@ -2,11 +2,27 @@
 $host = 'mysql'; //servicename from docker-compose.yml
 $user = 'mysqluser';
 $password = 'mysqlpassword';
-$db = 'my_db';
+$db = 'database';
 
 $connect = new mysqli($host, $user, $password, $db);
-if($connect->connect_error){
-  echo 'connection failed => ' . $connect->connect_error;
-} else {
-  echo 'successfully connected to mysql';
+
+if(!$connect){
+  die('connection failed => ' . mysqli_error());
 }
+
+echo 'successfully connected to mysql';
+$sql = 'SELECT first_name, last_name FROM users';
+$query = mysqli_query($connect, $sql);
+
+if (mysqli_num_rows($query) > 0) {
+  echo '<h2>Geniuses</h2>';
+  echo '<ol>';
+  while($row = mysqli_fetch_assoc($query)) {
+    echo '<li>' . $row['first_name'] . ' ' . $row['last_name'] . '</li>';
+  }
+  echo '</ol>';
+} else {
+  echo "Oops, there's no content to display.";
+}
+
+mysqli_close($connect);
